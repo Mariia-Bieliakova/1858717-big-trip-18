73f9bc5fbd42fa873c09ee41d6cc247e9e00f77c
Мария <mbeliakova92@gmail.com>
2022-09-30 18:34:59 +0200

    fix calculateTrip and offers click

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -3,71 +3,16 @@ import dayjs from 'dayjs';
 
 const MAX_DISPLAYED_DESTINATIONS = 3;
 
-const getDestinations = (points, destinations) => {
-  if (points.length === 0) {
-    return 'Your route';
-  }
-
-  let selectedDestinations = destinations
-    .filter((destination) => points
-      .find((point) => point.destination === destination.id));
-
-  selectedDestinations = selectedDestinations.map((destination) => destination.name);
-
-  if (selectedDestinations.length > MAX_DISPLAYED_DESTINATIONS) {
-    const firstDestination = destinations.find((destination) => points[0].destination === destination.id).name;
-    const lastDestination = destinations.find((destination) => points.at(-1).destination === destination.id).name;
-
-    return [firstDestination, lastDestination].join(' &mdash; ... &mdash; ');
-  }
-
-  return selectedDestinations.join(' &mdash; ');
-};
-
-const getTripValue = (points, offers) => {
-  if (points.length === 0) {
-    return 0;
-  }
-
-  const basePricesSum = points.reduce((total, point) => total + point.basePrice, 0);
-  let offersPriceSum = 0;
-
-  for (const point of points) {
-    const offersByType = offers.find((offer) => point.type === offer.type);
-
-    for (const offer of offersByType.offers) {
-      if (point.offers.includes(offer.id)) {
-        offersPriceSum += offer.price;
-      }
-    }
-  }
-
-  const fullTripPrice = basePricesSum + offersPriceSum;
-
-  return fullTripPrice;
-};
-
-const getTripDates = (points) => {
-  if (points.length === 0) {
-    return '... - ...';
-  }
-
-  const dateFrom = dayjs(points[0].dateFrom).format('D MMM');
-  const dateTo = dayjs(points.at(-1).dateTo).format('D MMM');
-
-  return [dateFrom, dateTo].join(' - ');
-};
-
-const createTripInfoTemplate = (points, offers, destinations ) => (
+const createTripInfoTemplate = (getTripDestinations, getTripDates, getTripPrice) => (
   `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
-      <h1 class="trip-info__title">${getDestinations(points, destinations)}</h1>
+      <h1 class="trip-info__title">${getTripDestinations()}</h1>
 
-      <p class="trip-info__dates">${getTripDates(points)}</p>
+      <p class="trip-info__dates">${getTripDates()}</p>
     </div>
 
     <p class="trip-info__cost">
-      Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTripValue(points, offers)}</span>
+      Total: &euro;&nbsp;<span class="trip-info__cost-value">${getTripPrice()}</span>
     </p>
   </section>`
 );
@@ -85,6 +30,58 @@ export default class TripInfoView extends AbstractView{
   }
 
   get template() {
-    return createTripInfoTemplate(this.#points, this.#offers, this.#destinations);
+    return createTripInfoTemplate(this.#getDestinations, this.#getTripDates, this.#getTripPrice);
   }
+
+  #getDestinations = () => {
+    if (!this.#points.length) {
+      return '';
+    }
+
+    const selectedDestinations = this.#destinations
+      .filter((destination) => this.#points
+        .find((point) => point.destination === destination.id))
+      .map((destination) => destination.name);
+
+    if (selectedDestinations.length > MAX_DISPLAYED_DESTINATIONS) {
+      const firstDestination = this.#destinations.find((destination) => this.#points[0].destination === destination.id).name;
+      const lastDestination = this.#destinations.find((destination) => this.#points.at(-1).destination === destination.id).name;
+
+      return [firstDestination, lastDestination].join(' &mdash; ... &mdash; ');
+    }
+
+    return selectedDestinations.join(' &mdash; ');
+  };
+
+  #getTripPrice = () => {
+    if (!this.#points.length) {
+      return '';
+    }
+
+    return this.#points.reduce((total, point) => {
+      const offersByType = this.#offers.find((offer) => point.type === offer.type);
+
+      const offersSum = offersByType.offers.reduce((acc, cur) => {
+        if (point.offers.includes(cur.id)) {
+          acc += cur.price;
+        }
+
+        return acc;
+      }, 0);
+
+      total += point.basePrice + offersSum;
+      return total;
+    }, 0);
+  };
+
+  #getTripDates = () => {
+    if (this.#points.length === 0) {
+      return '';
+    }
+
+    const dateFrom = dayjs(this.#points[0].dateFrom).format('D MMM');
+    const dateTo = dayjs(this.#points.at(-1).dateTo).format('D MMM');
+
+    return [dateFrom, dateTo].join(' - ');
+  };
 }
